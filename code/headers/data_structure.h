64ebb2a5b1933735be7af40ddb414ecae2712947
Seemingly <2781343172@qq.com>
2022-06-05 00:39:02 +0800

    enter

--- code/headers/data_structure.h
+++ code/headers/data_structure.h
@@ -1,11 +1,28 @@
 
 #ifndef DATA_STRUCTURE
 #define DATA_STRUCTURE
+#include"vector.h"
 
+
+//技能类型 
+typedef struct SKILL
+{
+    char* describe;     //武将技能描述
+    int effect;         //技能实际效果_标号    技能表  对应查找
+}skill;
+
+
+
+
+
+//武将类型
 typedef struct HERO
 {
-    
-}hero; 
+    char name[12];          //名称
+    char HP:4;              //血量
+    char nation:4;          //国家
+    vector* vector_skill;   //技能组
+}hero;
 
 
 
--- code/sources/main.c
+++ code/sources/main.c
@@ -17,33 +17,8 @@ int main()
     creat_error_log();
 
     //此处为程序测试
-    int arr[50];
-    for(int i=0;i<50;i++)
-    {
-        arr[i]=i;
-    }
 
-    vector vec;
-    vector_init(&vec,2,sizeof(int));
-    for(int i=0;i<50;i++)
-    {
-        if(i<10)
-        {
-            vector_pop(&vec);
-        }
-        else
-        {
-            vector_push_back(&vec,arr+i,co);
-        }
-        
-        
-    }
-    for(int i=0;i<vec.current_number_of_elements;i++)
-    {
-        printf("%d ",*(int*)vector_get_index(&vec,i));
-    }
-    printf("\n");
-    printf("%d %d\n",vec.total_number_of_elements,vec.current_number_of_elements);
+    
 
 
     //程序最后关闭日志文件
